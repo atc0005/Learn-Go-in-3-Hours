366ab8e7a864642277dcb0d20672fec3230d9a85
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 11:43:17 -0500

    Ch3 Adv Func - read parent scope var

--- classwork/ch3/advanced_functions/main.go
+++ classwork/ch3/advanced_functions/main.go
@@ -2,23 +2,16 @@ package main
 
 import "fmt"
 
-func addOne(a int) int {
-	return a + 1
-}
-
-func addTwo(a int) int {
-	return a + 2
-}
-
-// receive function as second argument. This second argument can be any
-// function that takes one integer argument and returns one integer
-func printOperation(a int, f func(int) int) {
-	fmt.Println(f(a))
-}
-
 func main() {
 
-	printOperation(1, addOne)
-	printOperation(1, addTwo)
+	b := 2
+
+	myAddOne := func(a int) int {
+
+		// read 'b' variable from main body of main()
+		return a + b
+	}
+
+	fmt.Println(myAddOne(1))
 
 }
