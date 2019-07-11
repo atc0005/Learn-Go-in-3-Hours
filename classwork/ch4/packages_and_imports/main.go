b80fae3c41dbd27e0f05c54dc9da850c98d52687
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 14:34:37 -0500

    Ch4 Packages - Show length of runes

--- classwork/ch4/packages_and_imports/main.go
+++ classwork/ch4/packages_and_imports/main.go
@@ -2,34 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"unicode/utf8"
 )
-
-// type rune = int32
-//     rune is an alias for int32 and is equivalent to int32 in all ways. It is
-//     used, by convention, to distinguish character values from integer values.
-func rot13(in rune) rune {
-	if in >= 'A' && in <= 'Z' {
-		return ((((in - 'A') + 13) % 26) + 'A')
-	}
-	if in >= 'a' && in <= 'z' {
-		return ((((in - 'a') + 13) % 26) + 'a')
-	}
-
-	// Return characters outside of A-Z and a-z as-is without modification
-	return in
-}
 
 func main() {
 
-	// Original string
-	s := "This is a test 123 😃"
+	s := "👋 🌍"
 
-	// Modified version of string
-	s2 := strings.Map(rot13, s)
-	fmt.Println(s2)
+	// Print the string
+	fmt.Println(s)
 
-	// Modified version of string converted back to original form
-	s3 := strings.Map(rot13, s2)
-	fmt.Println(s3)
+	// Print the length in bytes
+	fmt.Println(len(s))
+
+	// Print the number of "runes"
+	fmt.Println(utf8.RuneCountInString(s))
+
 }
