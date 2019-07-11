1aff1bde8a8a6f0222ea91a3448fe0a12cdcbac4
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 14:30:57 -0500

    Ch4 Packages - Annotate example

--- classwork/ch4/packages_and_imports/main.go
+++ classwork/ch4/packages_and_imports/main.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+// type rune = int32
+//     rune is an alias for int32 and is equivalent to int32 in all ways. It is
+//     used, by convention, to distinguish character values from integer values.
 func rot13(in rune) rune {
 	if in >= 'A' && in <= 'Z' {
 		return ((((in - 'A') + 13) % 26) + 'A')
@@ -12,13 +15,21 @@ func rot13(in rune) rune {
 	if in >= 'a' && in <= 'z' {
 		return ((((in - 'a') + 13) % 26) + 'a')
 	}
+
+	// Return characters outside of A-Z and a-z as-is without modification
 	return in
 }
 
 func main() {
+
+	// Original string
 	s := "This is a test 123 😃"
+
+	// Modified version of string
 	s2 := strings.Map(rot13, s)
 	fmt.Println(s2)
+
+	// Modified version of string converted back to original form
 	s3 := strings.Map(rot13, s2)
 	fmt.Println(s3)
 }
