e79aefe9d15154ec197c8c30a56f191b2b07e176
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 11:28:05 -0500

    Ch3 Functions - illustrate pass by value
    Show that variables passed to functions are passed by value
    (copies) and not by reference (the original variable). Variables
    passed to functions by value are shown to not be modified when
    access from main() after attempts to modify from the called
    function.

--- classwork/ch3/intro_to_functions/main.go
+++ classwork/ch3/intro_to_functions/main.go
@@ -6,25 +6,21 @@ import "fmt"
 // Go doesn't have optional parameters, named parameters or function
 // overloading
 
-func divAndRemainder(a int, b int) (int, int) {
-	return a / b, a % b
+func doubleFail(a int, arr [2]int, s string) {
+	a = a * 2
+	for i := 0; i < len(arr); i++ {
+		arr[i] = arr[i] * 2
+	}
+	s = s + s
+	fmt.Println("in doubleFail:", a, arr, s)
 }
-
-func addNumbers(a int, b int) int {
-	return a + b
-}
-
 func main() {
 
-	div, remainder := divAndRemainder(2, 3)
-	fmt.Println(div, remainder)
+	a := 1
+	arr := [2]int{2, 4}
+	s := "hello"
 
-	div, _ = divAndRemainder(10, 4)
-	fmt.Println(div)
-
-	_, remainder = divAndRemainder(100, -100)
-	fmt.Println(remainder)
-
-	divAndRemainder(-1, 20)
+	doubleFail(a, arr, s)
+	fmt.Println("in main:", a, arr, s)
 
 }
