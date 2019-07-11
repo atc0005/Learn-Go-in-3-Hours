34aa83b78f93441ac200fddf0e3c454e591ac6df
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 12:02:34 -0500

    Ch3 Adv Func - create closure

--- classwork/ch3/advanced_functions/main.go
+++ classwork/ch3/advanced_functions/main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
+// receive an int, return an anonymous function (which takes an int and
+// returns an int). This is a closure; the original value that was passed in
+// is kept as part of the state for later use.
+//
+// e.g., `fn := makeAddr(1); fmt.Println(fn(2))` results in three being printed
+func makeAdder(b int) func(int) int {
+	return func(a int) int {
+		return a + b
+	}
+}
+
 func main() {
 
-	b := 2
+	addOne := makeAdder(1)
+	addTwo := makeAdder(2)
 
-	// Closure: local function that has access to the variables that exist in
-	// the environment in which it was declared
-	myAddOne := func(a int) {
+	fn := makeAdder(1)
+	fmt.Println(fn(2))
 
-		// write to 'b' variable from main body of main()
-		b = a + b
-	}
-
-	myAddOne(1)
-	fmt.Println(b)
+	fmt.Println(addOne(1))
+	fmt.Println(addTwo(1))
 
 }
