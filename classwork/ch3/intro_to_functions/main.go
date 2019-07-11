0c6f19ebb527dd9038dd02426300dbc267127314
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 11:00:59 -0500

    Ch3 Functions - parameters

--- classwork/ch3/intro_to_functions/main.go
+++ classwork/ch3/intro_to_functions/main.go
@@ -1,13 +1,19 @@
 package main
 
+// Per Learn Go in 3 Hours course:
+// Go doesn't have optional parameters, named parameters or function
+// overloading
+
 import "fmt"
 
-func addNumbers() {
-	fmt.Println(2 + 3)
+func addNumbers(a int, b int) {
+	fmt.Println(a + b)
 }
 
 func main() {
 
-	addNumbers()
+	addNumbers(2, 3)
+	addNumbers(4, 10)
+	addNumbers(100, -100)
 
 }
