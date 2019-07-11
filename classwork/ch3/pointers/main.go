abf92bee889f3d320e6ad5c65885458c3bf2276a
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 14:10:40 -0500

    Ch3 Pointers - Show pitfall of new()

--- classwork/ch3/pointers/main.go
+++ classwork/ch3/pointers/main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func setTo10(a *int) {
+
+	// allocate new memory for a pointer and set a to reference that memory
+	// location in place of the memory location provided to setTo10 as a
+	// function parameter
+	a = new(int)
+
+	// Instead of changing the value stored in the original memory location
+	// (i.e., changing the original variable value), we are changing the value
+	// stored at the location created by our new of `new(int)`
 	*a = 10
 }
 
