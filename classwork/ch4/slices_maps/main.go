6ebf057b8ab369bcbd981e8dd3e07c928ecc73d0
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 16:15:48 -0500

    Ch4 Slices - strings, bytes, runes

--- classwork/ch4/slices_maps/main.go
+++ classwork/ch4/slices_maps/main.go
@@ -3,43 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	uniHello := "👋 🌍"
 
-	s3 := []string{"a", "b", "c"}
+	// my understanding: convert/cast copy of string to slice of bytes
+	// video: copy that string into a slice of bytes
+	bytes := []byte(uniHello)
 
-	// range returns an index, and the matching value for index
-	for k, v := range s3 {
-		fmt.Println(k, v)
-	}
+	// print slice of bytes (as numeric values, not a "string")
+	fmt.Println(bytes)
 
-	// create a slice from a slice using slice expression
-	//
-	// provide starting point and ending point for the slice, optionally
-	// leaving off starting point if starting point is 0 or the ending point
-	// if the ending point is the length of the slice
-	//
-	// WARNING: Both the original slice and the new slice refer to the same
-	// area of memory. Changing one changes the other. Slices are reference
-	// types and behave like pointers.
-	s4 := s3[0:2]
-	fmt.Println("s4:", s4)
+	// copy our string into a slice of runes
+	runes := []rune(uniHello)
 
-	s3[0] = "d"
-	fmt.Println("s4:", s4)
+	// print slice of runes
+	fmt.Println(runes)
 
-	// zero value for a slice is nil
-	// zero value for a pointer is nil
-	var s5 []string
-	s5 = s3
+	// NOTE: A slice of runes provides random access to the runes in a string
 
-	// Note: s5[1] is read out loud as "s 5 sub 1"
-	s5[1] = "camel"
-	fmt.Println("s3:", s3)
-
-	modSlice(s3)
-	fmt.Println("s3[0]:", s3[0])
-
-}
-
-func modSlice(s []string) {
-	s[0] = "hello"
+	// this modifies a copy, not the original string
+	runes[1] = 'a'
+	fmt.Println(runes)
+	fmt.Println(uniHello)
 }
