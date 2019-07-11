b0e3ae1ec2134c3c0a38e4e417f773a887c27957
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 11:40:36 -0500

    Ch3 Adv Func - pass func to func

--- classwork/ch3/advanced_functions/main.go
+++ classwork/ch3/advanced_functions/main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+func addOne(a int) int {
+	return a + 1
+}
+
+func addTwo(a int) int {
+	return a + 2
+}
+
+// receive function as second argument. This second argument can be any
+// function that takes one integer argument and returns one integer
+func printOperation(a int, f func(int) int) {
+	fmt.Println(f(a))
+}
+
 func main() {
 
-	// anonymous function (declaring function within a function is otherwise
-	// not allowed)
-	myAddOne := func(a int) int {
-		return a + 1
-	}
-
-	fmt.Println(myAddOne(1))
+	printOperation(1, addOne)
+	printOperation(1, addTwo)
 
 }
