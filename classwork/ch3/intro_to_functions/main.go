c3814978e7e150a5301ea482f9c9ac2b382a063d
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-11 11:02:33 -0500

    Ch3 Functions - return value

--- classwork/ch3/intro_to_functions/main.go
+++ classwork/ch3/intro_to_functions/main.go
@@ -1,19 +1,24 @@
 package main
 
+import "fmt"
+
 // Per Learn Go in 3 Hours course:
 // Go doesn't have optional parameters, named parameters or function
 // overloading
 
-import "fmt"
-
-func addNumbers(a int, b int) {
-	fmt.Println(a + b)
+func addNumbers(a int, b int) int {
+	return a + b
 }
 
 func main() {
 
-	addNumbers(2, 3)
-	addNumbers(4, 10)
-	addNumbers(100, -100)
+	a := addNumbers(2, 3)
+	fmt.Println(a)
+
+	a = addNumbers(4, 10)
+	fmt.Println(a)
+
+	a = addNumbers(100, -100)
+	fmt.Println(a)
 
 }
