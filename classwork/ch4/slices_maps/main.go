511fafe3e48d79150afc214e40dba57a466f6a56
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-23 07:47:32 -0500

    Ch4 - maps example 2

--- classwork/ch4/slices_maps/main.go
+++ classwork/ch4/slices_maps/main.go
@@ -4,35 +4,25 @@ import "fmt"
 
 func main() {
 
-	// https://blog.golang.org/go-maps-in-action
-	//
-	// var m map[string]int
-	//
-	// Map types are reference types, like pointers or slices, and so the
-	// value of m above is nil; it doesn't point to an initialized map. A nil
-	// map behaves like an empty map when reading, but attempts to write to a
-	// nil map will cause a runtime panic; don't do that. To initialize a map,
-	// use the built in make function:
-	m := make(map[string]int)
-	m["hello"] = 300
-
-	// store the value that the 'hello' key references
-	h := m["hello"]
-	fmt.Println("hello in m via 'h' variable:", h)
-
-	// this hasn't been set, so the zero value for this non-existent key in
-	// the map is used
-	fmt.Println("a in m:", m["a"])
-
-	// referred to as the "comma ok" idiom
-	// * 'v' contains the value (if present)
-	// * 'ok' contains the result of attempting to retrieve the value of the
-	//   specified key and is an indication of whether the key exists in the map
-	if v, ok := m["hello"]; ok {
-		fmt.Println("hello in m:", v)
+	m2 := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 50,
 	}
 
-	// overwrite the value already stored in the map via the 'hello' key
-	m["hello"] = 20
-	fmt.Println("hello in m:", m["hello"])
+	for k, v := range m2 {
+		fmt.Println(k, v)
+	}
+
+	// show that the key/value is present
+	fmt.Println("b in m2:", m2["b"])
+
+	// remove the key
+	delete(m2, "b")
+
+	// show that the key is no longer present
+	// note: zero value is returned
+	// presumably "comma ok" idiom is needed to confirm key is not present
+	// since a valid value for a key could be the zero value for the type
+	fmt.Println("b in m2:", m2["b"])
 }
