6548ae82ccc90d1a62f067df434d022944eb719d
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-23 07:44:07 -0500

    Ch4 - maps example 1

--- classwork/ch4/slices_maps/main.go
+++ classwork/ch4/slices_maps/main.go
@@ -3,25 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	uniHello := "👋 🌍"
 
-	// my understanding: convert/cast copy of string to slice of bytes
-	// video: copy that string into a slice of bytes
-	bytes := []byte(uniHello)
+	// https://blog.golang.org/go-maps-in-action
+	//
+	// var m map[string]int
+	//
+	// Map types are reference types, like pointers or slices, and so the
+	// value of m above is nil; it doesn't point to an initialized map. A nil
+	// map behaves like an empty map when reading, but attempts to write to a
+	// nil map will cause a runtime panic; don't do that. To initialize a map,
+	// use the built in make function:
+	m := make(map[string]int)
+	m["hello"] = 300
 
-	// print slice of bytes (as numeric values, not a "string")
-	fmt.Println(bytes)
+	// store the value that the 'hello' key references
+	h := m["hello"]
+	fmt.Println("hello in m via 'h' variable:", h)
 
-	// copy our string into a slice of runes
-	runes := []rune(uniHello)
+	// this hasn't been set, so the zero value for this non-existent key in
+	// the map is used
+	fmt.Println("a in m:", m["a"])
 
-	// print slice of runes
-	fmt.Println(runes)
+	// referred to as the "comma ok" idiom
+	// * 'v' contains the value (if present)
+	// * 'ok' contains the result of attempting to retrieve the value of the
+	//   specified key and is an indication of whether the key exists in the map
+	if v, ok := m["hello"]; ok {
+		fmt.Println("hello in m:", v)
+	}
 
-	// NOTE: A slice of runes provides random access to the runes in a string
-
-	// this modifies a copy, not the original string
-	runes[1] = 'a'
-	fmt.Println(runes)
-	fmt.Println(uniHello)
+	// overwrite the value already stored in the map via the 'hello' key
+	m["hello"] = 20
+	fmt.Println("hello in m:", m["hello"])
 }
