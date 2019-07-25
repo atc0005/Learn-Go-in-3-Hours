a5a249443ddd9f5032ca20226556141bb99b60ea
Adam Chalkley <atc0005@users.noreply.github.com>
2019-07-25 07:41:08 -0500

    Ch5 - Methods Example 2

--- classwork/ch5/methods/main.go
+++ classwork/ch5/methods/main.go
@@ -7,27 +7,50 @@ type Foo struct {
 	B string
 }
 
-// '(f Foo)' is a method receiver
-// ...
-// method receiver declared between the keyword func and the name of the
-// method. The method receiver looks like a parameter declaration. It consists
-// of a left parenthesis, and identifier, the type in which we are defining
-// the method, and a right parenthesis. Whenever you want to access a field
-// and a struct inside of this method you use this identifier. It is like the
-// self the first parameter and a Python method declaration or the implicit in
-// this reference that you get any Java method.
-//
-// Note: This is a value receiver (i.e., refer to Foo directly)
-func (f Foo) String() string {
-	return fmt.Sprintf("A: %d and B: %s", f.A, f.B)
+func (f Foo) fieldCount() int {
+	fmt.Println("    Inside Foo fieldCount() method")
+	return 2
 }
 
-// Double  does not have a return value since this method does in-place
-// modification of the method receiver
-//
-// Note: This is a "reference" receiver (due to pointer to Foo).
+func (f Foo) String() string {
+	fmt.Println("    Inside Foo String() method")
+	return fmt.Sprintf("%d fields: A: %d and B: %s", f.fieldCount(), f.A, f.B)
+}
+
 func (f *Foo) Double() {
+	fmt.Println("    Inside Foo Double() method")
 	f.A = f.A * 2
+}
+
+type Bar struct {
+	C bool
+
+	// embedded struct of type 'Foo'
+	Foo
+}
+
+func (b Bar) String() string {
+
+	// b.Foo.String() is a call to the 'String()' method on the embedded 'Foo'
+	//
+	// Q: I thought this was not needed/desired for embedded structs? e.g.,
+	//    the "is a" relationship?
+	// !: Note: If you try to call 'b.String()' here it results in a 'stack
+	//    overflow' error, presumably due to infinite recursion
+	//return fmt.Sprintf("%s and C: %v", b.Foo.String(), b.C)
+	fmt.Println("    Inside Bar String() method")
+	return fmt.Sprintf("%s and C: %v", b.Foo.String(), b.C)
+}
+
+// Note: Never going to be called from inside Foo; later when the
+// f.fieldCount() method is called it is called against Foo and not Bar; Foo
+// has no idea that Bar has a fieldCount() method, even with Foo embedded
+// inside of it. See notes below in main()
+//
+// This is confusing for users of languages where overriding is supported.
+func (b Bar) fieldCount() int {
+	fmt.Println("    Inside Bar fieldCount() method")
+	return 3
 }
 
 func main() {
@@ -37,13 +60,34 @@ func main() {
 		B: "Hello",
 	}
 
-	// Before modification
-	fmt.Println(f.String())
+	b := Bar{
+		C:   true,
+		Foo: f,
+	}
 
-	// Modify struct field
-	f.Double()
+	// This calls the String() method on 'b', which in turn explicitly calls
+	// the String() method on embedded 'f'.
+	//
+	// The String() method on embedded 'f' calls fieldCount() on method
+	// receiver 'f' instead of the fieldCount() method on 'b'. There is no
+	// inheritance in Go.
+	// Foo has no idea that there's a fieldCount() method on Bar as well even
+	// though Foo is embedded inside of it. That's because in Go there's no
+	// method of overriding and there's no virtual method dispatch. So the
+	// fieldCount() method that we declared on Bar is never going to be called
+	// from inside of Foo.
+	fmt.Println(b.String())
 
-	// After modification
-	fmt.Println(f.String())
+	// embedded access on Foo struct
+	// ... if there's a method declared on an embedded struct, we are able
+	// to access the method on the outer struct as though the method was
+	// declared on the outer struct.
+	//
+	// Note: Presumably this is due to the "is a" relationship (e.g., an
+	// "Android" is a "Person" with the same base traits)
+	b.Double()
+
+	// Same as earlier example
+	fmt.Println(b.String())
 
 }
