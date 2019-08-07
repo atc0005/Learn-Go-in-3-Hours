d162a5fc56892ac167d5ee59710ec89d2717578a
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-07 07:08:00 -0500

    Ch6 - Interfaces - Example 3

--- classwork/ch6/interfaces/main.go
+++ classwork/ch6/interfaces/main.go
@@ -2,35 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+func doStuff(i interface{}) {
 
-	// Empty interface; ANY type in Go matches it.
-	// The empty interface is like the "void" pointer in C/C++ or the "object"
-	// type in Java.
-	// This is Go's way of saying, "This could be anything".
+	// A type switch looks a great deal at this switch statement that we saw
+	// way back in section 3. For a type switch we have the keyword switch,
+	// and we do a type assertion like we saw in the previous example, only
+	// instead of using the name of a concrete type you put the keyword type
+	// itself in the parenthesis instead.
 	//
-	// Go provides two ways to get back to the concrete type behind an
-	// interface; there's no way to do anything else with an empty interface.
-	// 1) Type assertion
-	// 2) Type switch
-	var i interface{}
+	// So by convention, the variable on the left-hand side of a colon equals
+	// reuses the interface variables name on the right-hand side. And I agree
+	// this convention is a little bit confusing, but you get used to it after
+	// a bit.
+	switch i := i.(type) {
 
-	i = "Hello"
+	// each case statement has a type name on it
+	case int:
+		fmt.Println("Double i is", i+i)
+	case string:
+		fmt.Println("i is", len(i), "characters long")
 
-	// Type assertion. You can only do a type assertion on an interface. You
-	// can do a type conversion on any type at all.
-	j := i.(string)
+	// default indicates that we do not know what type is behind the interface
+	default:
+		fmt.Println("I don't know what to do with this")
+	}
+}
 
-	// If you specify an additional variable (which is "ok" by
-	// convention), ok will be assigned the value of true if the assertion
-	// worked and false if it didn't. If the assertion worked the first
-	// variable specified will get the value from the interface. If the
-	// assertion fails it will be assigned the zero value of that type.
-	k, ok := i.(int)
-	fmt.Println(j, k, ok)
-	// Hello 0 false
-
-	// panic: interface conversion: interface {} is string, not int
-	m := i.(int)
-	fmt.Println(m)
+func main() {
+	doStuff(10)
+	doStuff("Hello")
+	doStuff(true)
 }
