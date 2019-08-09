de127eb5d3b90b4d6eadcf9e1ccdfa08466e0d10
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-09 06:15:13 -0500

    Ch6 - Channels - Example 2 - Annotate
    Show that we can pass reading from a channel into the built-in
    append function without an intermediate variable (though this
    is likely not best practice).

--- classwork/ch6/channels/main.go
+++ classwork/ch6/channels/main.go
@@ -9,6 +9,10 @@ func main() {
 	// create a buffered channel that can be used (asynchronously) to hold up
 	// to 10 values before writes to the channel resume the normal
 	// synchronous/blocking behavior
+	//
+	// NOTE: Most of the time you will wish to use synchronous/unbuffered
+	// channels so that you can better understand/follow data flow (e.g.,
+	// "baton in a relay race")
 	out := make(chan int, 10)
 	for i := 0; i < 10; i++ {
 		go func(localI int) {
@@ -17,8 +21,12 @@ func main() {
 	}
 	var result []int
 	for i := 0; i < 10; i++ {
-		val := <-out
-		result = append(result, val)
+
+		// illustrating that we can read from the channel via the built-in
+		// `append()` function without the intermediate variable (though this
+		// is probably not near as clear as using the intermediate variable)
+		//val := <-out
+		result = append(result, <-out)
 	}
 	fmt.Println(result)
 }
