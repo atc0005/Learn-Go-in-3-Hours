cfb0cb8658e06b374304f21f3b9f9d3604edc887
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-08 23:09:07 -0500

    Ch6 - Goroutines - Example 2

--- classwork/ch6/goroutines/main.go
+++ classwork/ch6/goroutines/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func runMe() {
 	fmt.Println("Hello from a goroutine")
@@ -8,4 +11,5 @@ func runMe() {
 
 func main() {
 	go runMe()
+	time.Sleep(1 * time.Second)
 }
