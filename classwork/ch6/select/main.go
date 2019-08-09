791a7a4120bf17db6f989e577cd9258ea57e6d76
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-09 07:02:46 -0500

    Ch6 - Select - Example 1 - Annotate

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -6,9 +6,17 @@ func main() {
 	in := make(chan int)
 	out := make(chan int, 1)
 
+	// since this channel is buffered we're not blocked trying to write to
+	// the channel
 	out <- 1
 
+	// we ARE however blocked from writing to `in` since there are no other
+	// goroutines around to read from the channel (reminder: `main()` is also
+	// a goroutine)
 	in <- 2
+
+	// FIX: Use a `select` statement to manage channels
+
 	fmt.Println("wrote 2 to in")
 	v := <-out
 	fmt.Println("read", v, "from out")
