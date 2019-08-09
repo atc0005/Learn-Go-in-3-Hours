6d04b0ddc6cd7e55277aafdd476f8929da0404a2
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-09 06:39:52 -0500

    Ch6 - Channels - Example 3 - Fixed

--- classwork/ch6/channels/main.go
+++ classwork/ch6/channels/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	in := make(chan int)
@@ -13,9 +15,17 @@ func main() {
 		}
 	}()
 
+	// Write to the `in` channel twice before attempting to read from it
+	// in the goroutine. This causes a panic:
+	// fatal error: all goroutines are asleep - deadlock!
 	in <- 1
-	in <- 2
+	//	in <- 2
 	o1 := <-out
+
+	// The workaround is to make at least one of the channels buffered OR
+	// sequence the actions so that we write to `in`, then read from `out`
+	// before writing once more to `in` and so on.
+	in <- 2
 	o2 := <-out
 	fmt.Println(o1, o2)
 }
