8c684e955b1c7f6f1cd2280fc1a69c9cf51bb5e7
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-09 06:33:57 -0500

    Ch6 - Channels - Example 3 - Broken
    This initial version of example 3 writes to the `in` channel
    twice before reading from it.

--- classwork/ch6/channels/main.go
+++ classwork/ch6/channels/main.go
@@ -1,32 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
+	in := make(chan int)
+	out := make(chan int)
 
-	// create a buffered channel that can be used (asynchronously) to hold up
-	// to 10 values before writes to the channel resume the normal
-	// synchronous/blocking behavior
-	//
-	// NOTE: Most of the time you will wish to use synchronous/unbuffered
-	// channels so that you can better understand/follow data flow (e.g.,
-	// "baton in a relay race")
-	out := make(chan int, 10)
-	for i := 0; i < 10; i++ {
-		go func(localI int) {
-			out <- localI * 2
-		}(i)
-	}
-	var result []int
-	for i := 0; i < 10; i++ {
+	go func() {
+		for {
+			i := <-in
+			out <- i * 2
+		}
+	}()
 
-		// illustrating that we can read from the channel via the built-in
-		// `append()` function without the intermediate variable (though this
-		// is probably not near as clear as using the intermediate variable)
-		//val := <-out
-		result = append(result, <-out)
-	}
-	fmt.Println(result)
+	in <- 1
+	in <- 2
+	o1 := <-out
+	o2 := <-out
+	fmt.Println(o1, o2)
 }
