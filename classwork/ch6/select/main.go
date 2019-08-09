78ed923e2cd945dcd01c45d7fa48a0672963360f
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-09 07:09:07 -0500

    Ch6 - Select - Example 2

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -10,14 +10,22 @@ func main() {
 	// the channel
 	out <- 1
 
-	// we ARE however blocked from writing to `in` since there are no other
-	// goroutines around to read from the channel (reminder: `main()` is also
-	// a goroutine)
-	in <- 2
-
-	// FIX: Use a `select` statement to manage channels
-
-	fmt.Println("wrote 2 to in")
-	v := <-out
-	fmt.Println("read", v, "from out")
+	// A Select statement looks a lot like a switch statement, only the cases
+	// are reads and writes to channels. If any case can succeed that read or
+	// write happens and the commands for that case are executed. If we run
+	// this program now we see that we can skip over the case for `in` and go
+	// straight to the case for `out`, then we print out the message that we
+	// got from `out`.
+	//
+	// If more than one case in the Select statement can be run, because
+	// multiple channels are ready to be read or written, the Select statement
+	// picks one case at random to run. It does not go in order. By ensuring
+	// random behavior this prevents certain kinds of deadlocks that can
+	// happen in concurrent systems.
+	select {
+	case in <- 2:
+		fmt.Println("wrote 2 to in")
+	case v := <-out:
+		fmt.Println("read", v, "from out")
+	}
 }
