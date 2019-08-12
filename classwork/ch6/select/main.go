691104f47d9b45652205d2679aebdee3bc6177ba
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-11 23:40:01 -0500

    Ch6 - Select - Example 6 - Annotate

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -1,34 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func multiples(i int) chan int {
+func multiples(i int) (chan int, chan struct{}) {
 	out := make(chan int)
+	done := make(chan struct{})
 	curVal := 0
 	go func() {
 		for {
-			// infinite duration goroutine that continually writes an
-			// incrementing value down the channel
-			// Emphasis: IT NEVER ENDS
-			out <- curVal * i
-			curVal++
+			select {
+			case out <- curVal * i:
+				fmt.Printf("Current value is %d before post-increment.\n", curVal)
+				curVal++
+
+			// wait for a value to come down this channel; this channel is
+			// skipped as it is not usable until a value comes down the `done`
+			// channel
+			case <-done:
+				fmt.Println("goroutine shutting down")
+				return
+			}
 		}
-		// BUG: unreachable code (per go-vet)
-		fmt.Println("Is this ever reached?")
 	}()
 
-	// return the created channel that the goroutine will use to pass values
-	// down for use later
-	return out
+	// return a channel named `out` that the goroutine will use to pass values
+	// down for use late and a separtate channel named `done` that is used to
+	// control when the goroutine will end
+	return out, done
 }
 
 func main() {
-	twosCh := multiples(2)
+	twosCh, done := multiples(2)
 
-	// ranging over a channel that is closed will trigger the loop to end,
-	// but since the channel is not closed we need some other way to determine
-	// when to exit the for range loop. Here we use the returned value: when
-	// the value exceeds 20 we stop looping.
 	for v := range twosCh {
 		if v > 20 {
 			break
@@ -36,5 +42,16 @@ func main() {
 		fmt.Println(v)
 	}
 
+	// signal our goroutine that it should stop
+	close(done)
+	//
+	// Q: How quickly does it stop? Are we waiting for the "random" case for
+	// reading the `done` channel to hit or is it immediate?
+	//
+	// A: Brief testing shows that the curVal variable is incremented once
+	// more and sent down the channel (11), but is never read, so the rest
+	// of this application never sees it.
+
 	//do more stuff
+	time.Sleep(1 * time.Second)
 }
