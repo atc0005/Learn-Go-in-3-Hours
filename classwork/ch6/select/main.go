e9330e611f8ce210d8f33e813557737eba934793
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-12 06:47:41 -0500

    Ch6 - Select - Example 7 - Annoted

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -30,21 +30,45 @@ func main() {
 
 	go func() {
 		count := 0
+
+		// `count` is incremented inside each case statement after the
+		// associated channel is closed
 		for count < 2 {
+
+			// these case statements are only chosen if the associated channel
+			// is still open
 			select {
+
+			// use `comma, ok` idiom to check if the channel is closed
 			case i, ok := <-in:
+
+				// if the channel is closed ...
 				if !ok {
+					fmt.Println("in2 channel is:", ok)
 					count++
+
+					// the `in` channel is set to nil; while a closed
+					// channel returns a value immediately, a nil channel
+					// never returns. By setting this to nil, this effectively
+					// disables this case in the select from further use
 					in = nil
+					fmt.Println("in2 channel is:", ok)
 					continue
 				}
+
+				// if the channel is NOT closed ...
+				fmt.Println("in channel is:", ok)
 				fmt.Println("from in, result is", i*2)
+
 			case i, ok := <-in2:
 				if !ok {
+					fmt.Println("in2 channel is:", ok)
 					count++
 					in2 = nil
+					fmt.Println("in2 channel is:", ok)
 					continue
 				}
+				fmt.Println("in2 channel is:", ok)
 				fmt.Println("from in2, result is", i+2)
 			}
 		}
