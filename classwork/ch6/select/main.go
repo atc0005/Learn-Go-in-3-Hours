568ed4f35725b7699684d36bc201dd314a50ae33
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-11 23:12:50 -0500

    Ch6 - Select - Example 4 - Annotate

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	in := make(chan int, 1)
-	out := make(chan int, 1)
+	in := make(chan int)
+	out := make(chan int)
 
-	// since this channel is buffered we're not blocked trying to write to
-	// the channel
-	out <- 1
+	// since this channel is NOT buffered we would be deadlocked here after
+	// writing to the channel (while we wait for the value to be read from the
+	// channel)
+	// fatal error: all goroutines are asleep - deadlock!
+	//out <- 1
+	//
+	// Never reached!
+	//fmt.Println("Made it past sending 1 on the int typed `in` channel")
 
 	// A Select statement looks a lot like a switch statement, only the cases
 	// are reads and writes to channels. If any case can succeed that read or
@@ -27,11 +32,17 @@ func main() {
 	// write 2 to the `in` channel.
 	// Blocking writes cause this case to be skipped and the next one
 	// evaluated. To make this case eligible for use, we have to make sure the
-	// channel is buffered.
+	// channel is buffered. In this example, it is not.
 	case in <- 2:
 		fmt.Println("wrote 2 to in")
 		//fmt.Println(<-in)
 	case v := <-out:
 		fmt.Println("read", v, "from out")
+
+	// In this example, this is the ONLY eligible case statement due to the
+	// other unbuffered channels blocking
+	default:
+		fmt.Println("nothing else works")
 	}
+
 }
