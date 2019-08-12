3d5800e8b846ccfa65fa8f42b225bfce31a25a0e
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-12 07:05:30 -0500

    Ch6 - Select - Example 9 - Annotated

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -6,57 +6,67 @@ import (
 	"time"
 )
 
-// Simulate allowing more connections to service than we can handle. Later
-// example uses back-pressure to gracefully handle that connection overload.
+// Limit the total number of "connections" in order to provide
+// stable/consistent service handling
 
 func main() {
-	var wg sync.WaitGroup
-	totalStart := time.Now()
-	// got 49682 for 24841 after 1.153999s
-	// got 50884 for 25442 after 1.151998s
-	// got 50084 for 25042 after 1.153999s
-	// panic: too many concurrent operations on a single file or socket (max 1048575)
+	workers := 10000
 
-	// goroutine 1073951 [running]:
-	// internal/poll.(*fdMutex).rwlock(0xc000080280, 0x0, 0xc235ccfde8)
-	//         c:/go/src/internal/poll/fd_mutex.go:147 +0x146
-	// internal/poll.(*FD).writeLock(...)
-	//         c:/go/src/internal/poll/fd_mutex.go:239
-	// internal/poll.(*FD).Write(0xc000080280, 0xc25e4313c0, 0x29, 0x40, 0x0, 0x0, 0x0)
-	//         c:/go/src/internal/poll/fd_windows.go:659 +0x55
-	// os.(*File).write(...)
-	//         c:/go/src/os/file_windows.go:224
-	// os.(*File).Write(0xc00007e008, 0xc25e4313c0, 0x29, 0x40, 0x57dbc0, 0xc235ccfee0, 0x408ad2)
-	//         c:/go/src/os/file.go:145 +0x77
-	// fmt.Fprintln(0x4eb200, 0xc00007e008, 0xc235ccff48, 0x6, 0x6, 0x57cfe0, 0x3baa040c, 0x0)
-	//         c:/go/src/fmt/print.go:266 +0x92
-	// fmt.Println(...)
-	//         c:/go/src/fmt/print.go:275
-	// main.main.func1(0xbf4c729a85020f6c, 0xdba3eaa7d, 0x57cfe0, 0xc000052070, 0x1062fd)
-	//         T:/github/Learn-Go-in-3-Hours/classwork/ch6/select/main.go:22 +0x1a7
-	// created by main.main
-	//         T:/github/Learn-Go-in-3-Hours/classwork/ch6/select/main.go:18 +0x101
-	// exit status 2
-	//for i := 0; i < 100000000; i++ {
+	// create a buffered channel of functions that take an int and returns an
+	// int. The buffer size is set to the number of specified workers.
+	pool := make(chan func(int) int, workers)
+	for i := 0; i < workers; i++ {
+
+		// for each iteration, a function is put into the pool
+		pool <- func(in int) int {
+			// NOTE: Add intentional delay here to emulate a service unable to
+			// keep up with demand
+			time.Sleep(5 * time.Second)
+			result := 2 * in
+			return result
+		}
+	}
+
+	var wg sync.WaitGroup
+	count := 0
+	totalStart := time.Now()
 	for i := 0; i < 100000; i++ {
 		start := time.Now()
-		wg.Add(1)
-		go func(in int) {
 
-			// delay for one second
-			time.Sleep(1 * time.Second)
+		// try to get a worker from the pool
+		select {
 
-			// double the number
-			out := 2 * in
-			_ = out
+		// if successful, launch a goroutine, run the worker, then return the
+		// worker to the pool.
+		//
+		// Said another way, we try to read a "value" from the `pool` channel
+		// (which in this case is a function that receives an int and returns
+		// an int), save the function to `f`, then use `f` to perform a task
+		// before we send `f` back into the channel for reading in the next
+		// interation. The default case statement is used whenever a worker is
+		// not available (e.g., channel is empty, blocked from reading), or in
+		// other words, whenever we run so many goroutines that we cannot
+		// finish running them and returning them to keep the pool from
+		// running empty.
+		case f := <-pool:
+			fmt.Printf("ACCEPT | Workers available: %d\n", len(pool))
+			fmt.Println("processing", i)
+			count++
+			wg.Add(1)
+			go func(in int) {
+				out := f(in)
+				fmt.Println("got", out, "for", in, "after", time.Now().Sub(start))
+				pool <- f
+				wg.Done()
+			}(i)
 
-			// calculate the time needed
-			fmt.Println("got", out, "for", in, "after", time.Now().Sub(start))
-
-			// signal that the goroutine is finished
-			wg.Done()
-		}(i)
+		// reject the request if there is not an available worker
+		default:
+			fmt.Printf("REJECT | Workers available: %d\n", len(pool))
+			fmt.Println("rejecting request", i, "too busy")
+		}
 	}
 	wg.Wait()
+	fmt.Println("total processed:", count)
 	fmt.Println("total time:", time.Now().Sub(totalStart))
 }
