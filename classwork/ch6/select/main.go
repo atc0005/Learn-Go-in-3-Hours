59cde0c5dd26083025c4c6889301bef0d7eccc40
Adam Chalkley <atc0005@users.noreply.github.com>
2019-08-12 06:59:56 -0500

    Ch6 - Select - Example 8 - Annotated

--- classwork/ch6/select/main.go
+++ classwork/ch6/select/main.go
@@ -3,77 +3,60 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
+// Simulate allowing more connections to service than we can handle. Later
+// example uses back-pressure to gracefully handle that connection overload.
+
 func main() {
-	in := make(chan int)
-	in2 := make(chan int)
-
 	var wg sync.WaitGroup
-	wg.Add(3)
+	totalStart := time.Now()
+	// got 49682 for 24841 after 1.153999s
+	// got 50884 for 25442 after 1.151998s
+	// got 50084 for 25042 after 1.153999s
+	// panic: too many concurrent operations on a single file or socket (max 1048575)
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			in <- i
-		}
-		close(in)
-		wg.Done()
-	}()
+	// goroutine 1073951 [running]:
+	// internal/poll.(*fdMutex).rwlock(0xc000080280, 0x0, 0xc235ccfde8)
+	//         c:/go/src/internal/poll/fd_mutex.go:147 +0x146
+	// internal/poll.(*FD).writeLock(...)
+	//         c:/go/src/internal/poll/fd_mutex.go:239
+	// internal/poll.(*FD).Write(0xc000080280, 0xc25e4313c0, 0x29, 0x40, 0x0, 0x0, 0x0)
+	//         c:/go/src/internal/poll/fd_windows.go:659 +0x55
+	// os.(*File).write(...)
+	//         c:/go/src/os/file_windows.go:224
+	// os.(*File).Write(0xc00007e008, 0xc25e4313c0, 0x29, 0x40, 0x57dbc0, 0xc235ccfee0, 0x408ad2)
+	//         c:/go/src/os/file.go:145 +0x77
+	// fmt.Fprintln(0x4eb200, 0xc00007e008, 0xc235ccff48, 0x6, 0x6, 0x57cfe0, 0x3baa040c, 0x0)
+	//         c:/go/src/fmt/print.go:266 +0x92
+	// fmt.Println(...)
+	//         c:/go/src/fmt/print.go:275
+	// main.main.func1(0xbf4c729a85020f6c, 0xdba3eaa7d, 0x57cfe0, 0xc000052070, 0x1062fd)
+	//         T:/github/Learn-Go-in-3-Hours/classwork/ch6/select/main.go:22 +0x1a7
+	// created by main.main
+	//         T:/github/Learn-Go-in-3-Hours/classwork/ch6/select/main.go:18 +0x101
+	// exit status 2
+	//for i := 0; i < 100000000; i++ {
+	for i := 0; i < 100000; i++ {
+		start := time.Now()
+		wg.Add(1)
+		go func(in int) {
 
-	go func() {
-		for i := 100; i < 110; i++ {
-			in2 <- i
-		}
-		close(in2)
-		wg.Done()
-	}()
+			// delay for one second
+			time.Sleep(1 * time.Second)
 
-	go func() {
-		count := 0
+			// double the number
+			out := 2 * in
+			_ = out
 
-		// `count` is incremented inside each case statement after the
-		// associated channel is closed
-		for count < 2 {
+			// calculate the time needed
+			fmt.Println("got", out, "for", in, "after", time.Now().Sub(start))
 
-			// these case statements are only chosen if the associated channel
-			// is still open
-			select {
-
-			// use `comma, ok` idiom to check if the channel is closed
-			case i, ok := <-in:
-
-				// if the channel is closed ...
-				if !ok {
-					fmt.Println("in2 channel is:", ok)
-					count++
-
-					// the `in` channel is set to nil; while a closed
-					// channel returns a value immediately, a nil channel
-					// never returns. By setting this to nil, this effectively
-					// disables this case in the select from further use
-					in = nil
-					fmt.Println("in2 channel is:", ok)
-					continue
-				}
-
-				// if the channel is NOT closed ...
-				fmt.Println("in channel is:", ok)
-				fmt.Println("from in, result is", i*2)
-
-			case i, ok := <-in2:
-				if !ok {
-					fmt.Println("in2 channel is:", ok)
-					count++
-					in2 = nil
-					fmt.Println("in2 channel is:", ok)
-					continue
-				}
-				fmt.Println("in2 channel is:", ok)
-				fmt.Println("from in2, result is", i+2)
-			}
-		}
-		wg.Done()
-	}()
-
+			// signal that the goroutine is finished
+			wg.Done()
+		}(i)
+	}
 	wg.Wait()
+	fmt.Println("total time:", time.Now().Sub(totalStart))
 }
